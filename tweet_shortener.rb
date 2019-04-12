23adb60b45b8e0606c8e1a4682c72cfa4844c609
Daniel Sullivan <dansullivan19@gmail.com>
2019-04-12 22:19:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def dictionary
+  substitutes = {
+    "hello" => 'hi',
+    "to, two, too" =>'2', 
+    "for, four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
